04cb7f960b2ce2220617e90eef710ac8a805d857
Jakub Stachyra <jakubstachyra@wp.pl>
2024-12-21 19:13:44 +0100

    Add user class

---
+++ UserManagement/UserManagement.Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagement.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public bool IsActive { get; set; } = true;
+    public string PasswordHash { get; set; }
+}
