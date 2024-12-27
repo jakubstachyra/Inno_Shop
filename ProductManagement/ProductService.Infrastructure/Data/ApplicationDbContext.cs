1366b9965b7a6fe512a7239cd1c5d854fbaf62d8
Jakub Stachyra <jakubstachyra@wp.pl>
2024-12-27 15:14:37 +0100

    Add database connection.

---
+++ ProductManagement/ProductService.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductService.Domain.Entities;
+
+namespace ProductService.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasQueryFilter(p => !p.IsDeleted);
+        }
+    }
+
+}
