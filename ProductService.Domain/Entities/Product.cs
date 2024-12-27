d3e7356188880e48afa94527fc07c147983236e6
Jakub Stachyra <jakubstachyra@wp.pl>
2024-12-27 14:32:39 +0100

    Add productService project

---
+++ ProductService.Domain/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductService.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public int CreatorUserId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+
+}
